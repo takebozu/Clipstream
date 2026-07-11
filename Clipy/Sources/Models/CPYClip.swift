93c484ca6f940a0ad1ebf5b6d43caaec7c84166e
Satoshi Takezawa <takechanx@gmail.com>
2026-07-11 21:32:51 +0900

    Fixed an issue where copying would fail after prolonged use

--- Clipy/Sources/Models/CPYClip.swift
+++ Clipy/Sources/Models/CPYClip.swift
@@ -19,7 +19,12 @@ final class CPYClip {
     // MARK: - Properties
     var dataPath: String = ""
     var title: String = ""
+    /// Guaranteed-unique identifier used as the menu's lookup key.
     @Attribute(.unique) var dataHash: String = ""
+    /// Stable hash of the clip's content, used for duplicate detection and
+    /// "overwrite same history". Unlike `dataHash` this is intentionally NOT
+    /// unique, so it can never trigger a constraint conflict on save.
+    var contentHash: String = ""
     var primaryType: String = ""
     var updateTime: Int = 0
     var thumbnailPath: String = ""
@@ -28,11 +33,12 @@ final class CPYClip {
     init() {}
 
     init(dataPath: String = "", title: String = "", dataHash: String = "",
-         primaryType: String = "", updateTime: Int = 0,
+         contentHash: String = "", primaryType: String = "", updateTime: Int = 0,
          thumbnailPath: String = "", isColorCode: Bool = false) {
         self.dataPath = dataPath
         self.title = title
         self.dataHash = dataHash
+        self.contentHash = contentHash
         self.primaryType = primaryType
         self.updateTime = updateTime
         self.thumbnailPath = thumbnailPath
